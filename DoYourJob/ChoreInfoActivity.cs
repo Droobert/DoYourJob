8fb3d7ee1b4e906f1befe0b173579f08b0855113
Andrew Doyle <droodoyle89@gmail.com>
2017-11-06 23:16:03 -0500

    Added reminders for Chores
    The reminders are currently not customizable as to when they go off. They always go off 5 seconds after you push the button, but it's a start.

--- DoYourJob/ChoreInfoActivity.cs
+++ DoYourJob/ChoreInfoActivity.cs
@@ -16,6 +16,8 @@ namespace DoYourJob
     [Activity(Label = "ChoreInfoActivity")]
     public class ChoreInfoActivity : Activity
     {
+        Button setReminderButton;
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
@@ -33,6 +35,28 @@ namespace DoYourJob
             FindViewById<TextView>(Resource.Id.nameTextDisplay).Text = choreCollection[index].name;
             FindViewById<TextView>(Resource.Id.dateTextDisplay).Text = choreCollection[index].date;
             FindViewById<TextView>(Resource.Id.detailsTextDisplay).Text = choreCollection[index].details;
+
+            setReminderButton = FindViewById<Button>(Resource.Id.setReminderButton);
+            setReminderButton.Click += (sender, e) =>
+            {
+                Remind(DateTime.Parse(choreCollection[index].date), choreCollection[index].name, choreCollection[index].details);
+            };
+        }
+
+        public void Remind(DateTime dateTime, string title, string message)
+        {
+
+            Intent alarmIntent = new Intent(this, typeof(AlarmReceiver));
+            alarmIntent.PutExtra("message", message);
+            alarmIntent.PutExtra("title", title);
+
+            PendingIntent pendingIntent = PendingIntent.GetBroadcast(this, 0, alarmIntent, PendingIntentFlags.UpdateCurrent);
+            AlarmManager alarmManager = (AlarmManager)GetSystemService(Context.AlarmService);
+
+            //TODO: For demo set after 5 seconds.
+            //TODO: Set alarm to go off at a selected time of a specific day.
+            alarmManager.Set(AlarmType.ElapsedRealtime, SystemClock.ElapsedRealtime() + 5 * 1000, pendingIntent);
+
         }
     }
 }
